1e90ab31fd1910fe7e3f7945005ae9d390bf91d1
phzsantos <phzsantos2002@gmail.com>
2022-01-18 21:01:34 -0300

    Exercise 52 refactored

--- 00 - Introducao/Exercicios/52 - Exercicio.c
+++ 00 - Introducao/Exercicios/52 - Exercicio.c
@@ -2,33 +2,34 @@
 
 int main()
 {
-	double valor_base, valor_premio, primeiro, segundo, terceiro,
-	       porcentagem_primeiro, porcentagem_segundo, porcentagem_terceiro,
-		   primeiro_recebe, segundo_recebe, terceiro_recebe;
+	int i;
+	double valor_base, valor_premio, valores_investidos[3], porcentagens[3], valores_recebidos[3];
 
 	printf("Valor base da aposta: ");
 	scanf("%lf", &valor_base);
 	printf("Valor do premio: ");
 	scanf("%lf", &valor_premio);
 
-	printf("Valor do primeiro amigo: ");
-	scanf("%lf", &primeiro);
-	printf("Valor do segundo amigo: ");
-	scanf("%lf", &segundo);
-	printf("Valor do terceiro amigo: ");
-	scanf("%lf", &terceiro);
+	for (i = 0; i < 3; i++)
+	{
+		printf("Valor do %dº amigo: ", i+1);
+		scanf("%lf", &valores_investidos[i]);
+	}
 
-	porcentagem_primeiro = (primeiro * 100) / valor_base;
-	porcentagem_segundo = (segundo * 100) / valor_base;
-	porcentagem_terceiro = (terceiro * 100) / valor_base;
+	for (i = 0; i < 3; i++)
+	{
+		porcentagens[i] = (valores_investidos[i] * 100) / valor_base;
+	}
+	
+	for (i = 0; i < 3; i++)
+	{
+		valores_recebidos[i] = valor_premio * porcentagens[i] / 100;
+	}
 
-	primeiro_recebe = valor_premio * porcentagem_primeiro / 100;
-	segundo_recebe = valor_premio * porcentagem_segundo / 100;
-	terceiro_recebe = valor_premio * porcentagem_terceiro / 100;
-
-	printf("Primeiro recebe: %.2lf\n", primeiro_recebe);
-	printf("Segundo recebe: %.2lf\n", segundo_recebe);
-	printf("Terceiro recebe: %.2lf\n", terceiro_recebe);
+	for (i = 0; i < 3; i++)
+	{
+		printf("Valor que o %dº recebe: %.2lf\n", i+1, valores_recebidos[i]);
+	}
 
 	return 0;
 }
