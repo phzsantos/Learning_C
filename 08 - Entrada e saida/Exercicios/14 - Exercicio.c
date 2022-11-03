ef981332e4f45ace5eb133a99b63465b7a3dca2b
phzsantos <phzsantos2002@gmail.com>
2022-11-02 22:40:41 -0300

    Update exercise 14

--- 08 - Entrada e saida/Exercicios/14 - Exercicio.c
+++ 08 - Entrada e saida/Exercicios/14 - Exercicio.c
@@ -1,27 +1,61 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 int main()
 {
-    int dia, mes, ano;
-    char nome_arquivo[30], linha[50];
+    int data, dia_atual, mes_atual, ano_atual, posicao_do_primeiro_numero;
+    char data_string[9], dia_string[3], nome_arquivo[30], linha[50];
 
     FILE* arquivo;
 
     printf("Arquivo: ");
     scanf("%s", &nome_arquivo);
+    
+    printf("Dia: ");
+    scanf("%d", &dia_atual);
+    printf("Mes: ");
+    scanf("%d", &mes_atual);
+    printf("Ano: ");
+    scanf("%d", &ano_atual);
 
     arquivo = fopen(nome_arquivo, "r");
 
-    printf("Dia: ");
-    scanf("%d", &dia);
-    printf("Mes: ");
-    scanf("%d", &mes);
-    printf("Ano: ");
-    scanf("%d", &ano);
-
     if (arquivo)
     {
+        while (!feof(arquivo))
+        {
+            fgets(linha, 50, arquivo);
+            // char * splited = strtok(linha, " ");
+            // while (splited != NULL)
+            // {
+            //     printf("%s\n", splited);
+            //     splited = strtok(NULL, " ");
+            // }
+            // for (int i = 0; i < strlen(linha); i++)
+            // {
+            //     if (linha[i] == '0' ||
+            //         linha[i] == '1' ||
+            //         linha[i] == '2' ||
+            //         linha[i] == '3' ||
+            //         linha[i] == '4' ||
+            //         linha[i] == '5' ||
+            //         linha[i] == '6' ||
+            //         linha[i] == '7' ||
+            //         linha[i] == '8' ||
+            //         linha[i] == '9')
+            //     {
+            //         posicao_do_primeiro_numero = i;
+            //     }
+            // }
+            // data = atoi(&linha[posicao_do_primeiro_numero-7]);
+            // printf("%d\n", data);
 
+            // sprintf(data_string, "%d", data);
+
+            // strcpy(dia_string, &data_string[1]);
+            // printf("%s\n", dia_string);
+        }
     }
 
     fclose(arquivo);
