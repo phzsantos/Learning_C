2b6f1ff46da2c608ed55bec852c11b02b219a7d3
phzsantos <phzsantos2002@gmail.com>
2023-05-07 22:46:03 -0300

    Exercise 14

--- 08 - Entrada e saida/Exercicios/14 - Exercicio.c
+++ 08 - Entrada e saida/Exercicios/14 - Exercicio.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 int main()
 {
-    int data, dia_atual, mes_atual, ano_atual, posicao_do_primeiro_numero;
-    char data_string[9], dia_string[3], nome_arquivo[30], linha[50];
+    int i, dia_atual, mes_atual, ano_atual, dia, mes, ano, idade;
+    char nome_arquivo[30], linha[50], nome[30];
 
     FILE* arquivo;
+    FILE* arquivo_saida;
 
     printf("Arquivo: ");
-    scanf("%s", &nome_arquivo);
+    scanf("%s", nome_arquivo);
     
     printf("Dia: ");
     scanf("%d", &dia_atual);
@@ -20,42 +22,34 @@ int main()
     scanf("%d", &ano_atual);
 
     arquivo = fopen(nome_arquivo, "r");
+    arquivo_saida = fopen("idades.txt", "w");
 
-    if (arquivo)
+    if (arquivo && arquivo_saida)
     {
-        while (!feof(arquivo))
+        while (fgets(linha, 50, arquivo) != NULL)
         {
-            fgets(linha, 50, arquivo);
-            // char * splited = strtok(linha, " ");
-            // while (splited != NULL)
-            // {
-            //     printf("%s\n", splited);
-            //     splited = strtok(NULL, " ");
-            // }
-            // for (int i = 0; i < strlen(linha); i++)
-            // {
-            //     if (linha[i] == '0' ||
-            //         linha[i] == '1' ||
-            //         linha[i] == '2' ||
-            //         linha[i] == '3' ||
-            //         linha[i] == '4' ||
-            //         linha[i] == '5' ||
-            //         linha[i] == '6' ||
-            //         linha[i] == '7' ||
-            //         linha[i] == '8' ||
-            //         linha[i] == '9')
-            //     {
-            //         posicao_do_primeiro_numero = i;
-            //     }
-            // }
-            // data = atoi(&linha[posicao_do_primeiro_numero-7]);
-            // printf("%d\n", data);
+            for (i = 0; !isdigit(linha[i]); i++)
+            {
+                nome[i] = linha[i];
+            }
+            nome[i] = '\0';
 
-            // sprintf(data_string, "%d", data);
+            sscanf(&linha[i], "%d %d %d", &dia, &mes, &ano);
 
-            // strcpy(dia_string, &data_string[1]);
-            // printf("%s\n", dia_string);
+            idade = ano_atual - ano;
+
+            if (mes > mes_atual || (mes == mes_atual && dia > dia_atual))
+            {
+                idade--;
+            }
+
+            fprintf(arquivo_saida, "%s %d\n", nome, idade);
         }
+        fclose(arquivo_saida);
+    }
+    else
+    {
+        printf("Erro ao abrir o arquivo!\n");
     }
 
     fclose(arquivo);
