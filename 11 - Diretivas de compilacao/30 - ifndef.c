73e2ada0972275e0c1975f095a98eaa927481f77
phzsantos <phzsantos2002@gmail.com>
2024-06-14 21:10:13 -0300

    ifndef

---
+++ 11 - Diretivas de compilacao/30 - ifndef.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+#ifndef PI
+    #define PI 3.1415
+#endif
+
+int main()
+{
+    int valor = 5;
+
+    valor = 467;
+
+    printf("Valor: %d\n", valor);
+
+    printf("PI: %f\n", PI);
+
+    return 0;
+}
