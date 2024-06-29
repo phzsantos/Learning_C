84b05bbc5bbd7ca8ed8490499c241eb4a0c9160a
phzsantos <phzsantos2002@gmail.com>
2024-06-28 22:30:26 -0300

    Select sort

---
+++ 15 - Ordenacao de vetores/47 - Select sort.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+void select_sort(int vetor[], int tamanho)
+{
+    int menor, troca;
+
+    for (int i = 0; i < tamanho; i++)
+    {
+        menor = i;
+        for (int j = i + 1; j < tamanho; j++)
+        {
+            if (vetor[j] < vetor[menor])
+            {
+                menor = j;
+            }
+        }
+
+        if (i != menor)
+        {
+            troca = vetor[i];
+            vetor[i] = vetor[menor];
+            vetor[menor] = troca;
+        }
+    }
+}
+
+int main()
+{
+    int vetor[6] = {8, 3, 1, 42, 12, 5};
+
+    select_sort(vetor, 6);
+
+    for (int i = 0; i < 6; i++)
+    {
+        printf("%d ", vetor[i]);
+    }
+
+    return 0;
+}
