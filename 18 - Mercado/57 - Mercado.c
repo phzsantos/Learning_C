24f3ff051b706c6f2ee2d19b4eaf241f40d0bbba
phzsantos <phzsantos2002@gmail.com>
2024-07-03 17:01:17 -0300

    Structuring market

---
+++ 18 - Mercado/57 - Mercado.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+typedef struct
+{
+    int codigo;
+    char nome[30];
+    float preco;
+} Produto;
+
+typedef struct
+{
+    Produto produto;
+    int quantidade;
+} Carrinho;
+
+void infoProduto(Produto prod);
+void menu();
+void cadastrarProduto();
+void listarProdutos();
+void vizualizarCarrinho();
+Produto pegarProdutoPorCodigo(int codigo);
+int * temNoCarrinho(int codigo);
+void fecharPedido();
+
+static int contador_produto = 0;
+static int contador_carrinho = 0;
+static Carrinho carrinho[50];
+static Produto produtos[50];
+
+int main()
+{
+    menu();
+
+    return 0;
+}
+
+void infoProduto(Produto prod)
+{
+
+}
+
+void cadastrarProduto()
+{
+
+}
+
+void visualizarCarrinho()
+{
+
+}
+
+Produto pegarProdutoPorCodigo(int codigo)
+{
+
+}
+
+int * temNoCarrinho(int codigo)
+{
+
+}
+
+void fecharPedido()
+{
+
+}
